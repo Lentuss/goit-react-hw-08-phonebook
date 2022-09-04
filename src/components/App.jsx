1d9694deddcc4a5e29752b9d16835895729f55fd
Olena Pohybil <itlentus@gmail.com>
2022-09-04 13:01:38 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import LogIn from 'pages/LogIn/LogIn';
+
 const Layout = lazy(() => import('../components/Layout/Layout'));
 const Home = lazy(() => import('../pages/Home/Home'));
 const Contacts = lazy(() => import('../pages/Contacts/Contacts'));
 const Register = lazy(() => import('../pages/Register/Register'));
-const LogIn = lazy(() => import('../pages/LogIn/LogIn'));
+// const LogIn = lazy(() => import('../pages/LogIn/LogIn'));
 
 const App = () => {
   return (
