62c13116d50656853720af099eefda5ed42aee18
Olena Pohybil <itlentus@gmail.com>
2022-09-04 23:02:36 +0300

    del logs

--- src/redux/auth/authSlise.js
+++ src/redux/auth/authSlise.js
@@ -11,30 +11,29 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
-    [register.pending]: (state, action) => console.log(action),
+    [register.pending]: (state, action) => state,
     [register.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
     [register.rejected]: (state, action) => action.payload,
-    [logIn.pending]: (state, action) => console.log(action),
+    [logIn.pending]: (state, action) => state,
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
     [logIn.rejected]: (state, action) => action.payload,
-    [logOut.pending]: (state, action) => console.log(action),
+    [logOut.pending]: (state, action) => state,
     [logOut.fulfilled](state, action) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
     },
     [logOut.rejected]: (state, action) => action.payload,
-    [fetchUser.pending]: (state, action) => console.log(action),
+    [fetchUser.pending]: (state, action) => state,
     [fetchUser.fulfilled](state, action) {
-      // console.log(action);
       state.user = action.payload;
       state.isLoggedIn = true;
     },
